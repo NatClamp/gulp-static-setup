a3f9d1a40ce4bc825434fa562193ce97633f3956
Natalie Clamp <nat.clamp@gmail.com>
2019-05-31 13:32:54 +0100

    Adding gulp tasks for compiling nunjucks, watching for HTML changes and linting

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
 const cleanCss = require("gulp-clean-css");
 const imagemin = require("gulp-imagemin")
+const eslint = require("gulp-eslint");
+const nunjucks = require("gulp-nunjucks");
+const fs = require("fs");
+const yaml = require("yaml")
 
 sass.compiler = require('node-sass')
 
@@ -13,6 +17,15 @@ const runServer = () => {
             baseDir: "./public"
         }
     })
+}
+
+// HTML
+
+// Compile HTML
+const compileHTML = () => {
+    return src("./app/src/views/*.html")
+        .pipe(nunjucks.compile(yaml.parse(fs.readFileSync("./content.yml", "utf8"))))
+        .pipe(dest("./public/views"))
 }
 
 // SCSS //
@@ -49,11 +62,26 @@ const watchImages = () => {
     .on("change", browserSync.reload)
 }
 
+// Watch for any changes to HTML files
+const watchHTML = () => {
+    watch("./app/src/views.*.html", compileHTML)
+        .on("change", browserSync.reload)
+}
+
 // Watch for any changes to the sass folder
 const watchSass = () => {
     return watch('./app/src/sass/**', compileSass);
 }
 
+// LINTING
 
-exports.build = series(compileSass, miniSass, compressImgs)
-exports.default = parallel(runServer, watchSass, watchImages)
+const lint = () => {
+    return src("./app/src/js/*.js")
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+}
+
+exports.lint = lint;
+exports.build = series(lint, compileHTML, compileSass, miniSass, compressImgs)
+exports.default = parallel(runServer, watchHTML, watchSass, watchImages)
